cb2d2213ca91d49e5c438e45c20821bc19f9e80a
David1709 <davidsilva1709@gmail.com>
2020-12-30 21:11:33 +0000

    Added movies related functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const movieController = require('../app/controllers/movieController');
+
 const routes = express.Router();
 
-routes.get('/', function(req, res) {
-  return res.render('home/index.njk');
-});
-
-
+routes.get('/', movieController.getAll);
+routes.post('/movies', movieController.create);
 
 routes.get('/managing/movies/create', function(req, res) {
   return res.render('admin/registreMovie.njk');
