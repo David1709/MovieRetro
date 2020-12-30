5ef0ad2b58a385388ef40749fec11980448b4ed8
David1709 <davidsilva1709@gmail.com>
2020-12-30 21:07:49 +0000

    Added routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', function(req, res) {
-  return res.render('layout.njk');
+  return res.render('home/index.njk');
+});
+
+
+
+routes.get('/managing/movies/create', function(req, res) {
+  return res.render('admin/registreMovie.njk');
 });
 
 module.exports = routes;
