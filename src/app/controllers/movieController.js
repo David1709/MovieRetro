f5f405ee48e62ced25d177f9eaa2bd6e494991e3
David1709 <davidsilva1709@gmail.com>
2021-01-01 16:21:24 +0000

    Added some debugging code and formatted the document

--- src/app/controllers/movieController.js
+++ src/app/controllers/movieController.js
@@ -5,23 +5,27 @@ const { v4 } = require('uuid');
 const Movie = require('../models/Movie');
 
 module.exports = {
-  create (req, res) {
-    const keys = Object.keys(req.body);
+    create(req, res) {
+        const keys = Object.keys(req.body);
 
-    for (key of keys) {
-      if (req.body[key] == '') return res.send('Please, fill all fields!');
+        for (key of keys) {
+            if (req.body[key] == '') return res.send('Please, fill all fields!');
+        }
+
+        db.movies.push({ id: v4(), ...req.body });
+
+        //console.log("writing data");
+
+        fs.writeFile('database.json', JSON.stringify(db, null, 2), (err) => {
+            if (err) return res.send("Write file error");
+
+            // console.log("movie added");
+            return res.redirect('/');
+        });
+    },
+
+    getAll(req, res) {
+        //console.log("renderi index");
+        return res.render('home/index.njk', { movies: db.movies });
     }
-
-    db.movies.push({id: v4(), ...req.body});
-
-    fs.writeFile('database.json', JSON.stringify(db, null, 2), (err) => {
-      if (err) return res.send("Write file error");
-
-      return res.redirect('/');
-    });
-  },
-
-  getAll (req, res) {
-    return res.render('home/index.njk', { movies: db.movies });
-  }
 }
