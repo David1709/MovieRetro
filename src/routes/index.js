3cd0dc4976a35c924c24082c5a6b6d5eb2fee640
David1709 <davidsilva1709@gmail.com>
2021-01-03 21:50:07 +0000

    Improved login link

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const movieController = require('../app/controllers/movieController');
+const loginController = require('../app/controllers/loginController');
 
 const routes = express.Router();
 
 routes.get('/', movieController.getAll);
 routes.post('/movies', movieController.create);
+routes.post('/login', loginController.login);
+routes.post('/register', registerController.register);
 
 routes.get('/managing/movies/create', function(req, res) {
   return res.render('admin/registreMovie.njk');
+});
+
+routes.get('/login/', function(req, res) {
+  return res.render('login.njk');
+});
+
+routes.get('/register/', function(req, res) {
+  return res.render('register.njk');
 });
 
 module.exports = routes;
